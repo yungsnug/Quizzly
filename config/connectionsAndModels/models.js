010b9a664368c748116e4b2ca34ad838d47fc24f
Zachary Zeff <lightning@guest-wireless-upc-1602-10-120-033-247.usc.edu>
2016-02-29 11:17:13 -0800

    barebone files in folder config/connectionsAndModels/*

---
+++ config/connectionsAndModels/models.js
@@ -0,0 +1,18 @@
+/**
+ * Default model configuration
+ * (sails.config.models)
+ *
+ * Unless you override them, the following properties will be included
+ * in each of your models.
+ *
+ * For more info on Sails models, see:
+ * http://sailsjs.org/#!/documentation/concepts/ORM
+ */
+
+module.exports.models = {
+  /*need to run npm install sails-mysql and have sql installed (follow guide)*/
+  // connection: 'mysql',
+  /*need to run npm install sails-memory (follow guide)*/
+  connection: 'memory',
+  migrate: 'alter'
+};
