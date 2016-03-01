2deb7ed1b0c4524409687365c7290ee7176d2196
Zachary Zeff <lightning@Zacharys-MacBook-Pro.local>
2016-02-29 23:10:54 -0800

    Added login without sessions and added route

--- api/controllers/ProfessorController.js
+++ api/controllers/ProfessorController.js
@@ -16,6 +16,33 @@ module.exports = {
   },
 
   /**
+   * UserController.login()
+   */
+  login: function(req, res) {
+    var params = req.params.all();
+    //or
+    // var emailid = req.param('email');
+    // var password = req.param('password');
+    
+    // Authentication code here
+    
+
+
+
+Professor.find({email: params.email, password: params.password}).exec(function(err){
+      if (err) {
+                sails.log.debug('Some error occured ' + err);
+                return res.json(500, { error: 'Some error occured' });
+      }
+      console.log("hello");
+      // req.session.userId =
+     return res.ok();
+   
+ });
+  
+},
+
+  /**
    * `ProfessorController.getAnswers()`
   */
   getAnswers: function (req, res) {
