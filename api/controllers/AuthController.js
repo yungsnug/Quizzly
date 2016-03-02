6752330fe8e9e1c1ebfd3d84356cc7930cedc502
Conner Frey <connerfrey@Conners-MacBook-Pro-2.local>
2016-03-02 12:43:42 -0800

    Fixed the AuthController but still error

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -5,6 +5,8 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var Promise = require('q');
+
 module.exports = {
   user: function(req, res) {
     console.log("req.session", req.session);
@@ -14,5 +16,31 @@ module.exports = {
     } else {
       console.log("session isn't set");
     }
+  },
+  login: function(req, res) {
+    var data = req.params.all();
+    Promise.all([
+      Professor.find({email: data.email, password: data.password}),
+      Student.find({email: data.email, password: data.password})
+
+    ]).spread(function(professor, student){
+      var user = {};
+      if(professor.length > 0) {
+        user = professor;
+      }
+
+      if(student.length > 0) {
+        user = student;
+      }
+
+      user.password = "";
+
+      req.session.user = user;
+      res.json(professor);
+    }).catch(function(){
+      console.log("error is encountered");
+    }).done(function(){
+      console.log("promise call is done");
+    });
   }
 };
