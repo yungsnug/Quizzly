12d2d224020d9185f0eaa01836c76c00bceb7bde
Zachary Zeff <lightning@Zacharys-MacBook-Pro.local>
2016-03-04 18:30:38 -0800

    added controller and model StudentAnswer

---
+++ api/controllers/StudentAnswerController.js
@@ -0,0 +1,11 @@
+/**
+ * StudentAnswerController
+ *
+ * @description :: Server-side logic for managing Studentanswers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
