5ce4b47b803365c7f22d960c60ee867948619cf3
Mark Krant <mark.krant@gmail.com>
2016-04-12 19:43:59 -0700

    added question count to pushes

--- api/controllers/QuestionController.js
+++ api/controllers/QuestionController.js
@@ -85,6 +85,7 @@ module.exports = {
                "quest_id": question.id,
                "type": question.type,
                "time_limit": question.duration,
+               "num_answers": question.answers.length               
              }
            },
            "ios": {
@@ -94,6 +95,8 @@ module.exports = {
                "quest_id": question.id,
                "type": question.type,
                "time_limit": question.duration,
+               "num_answers": question.answers.length
+            //   "answers": []
              }
            }
         },
@@ -118,7 +121,7 @@ module.exports = {
           if (err) {
               // Handle error
               console.log(err);
-              return;
+              return res.json({ error: "Push not sent"});
           }
           console.log(data);
           return res.json({
@@ -134,7 +137,12 @@ module.exports = {
     var question_id = req.query.question;
     var section_id = req.query.section;
     Section.findOne({id: section_id}).populate('students').exec(function(err, section) {
-
+      if(err) {
+        return res.json({error: res.negotiate(err)})
+      }
+      if(!section) {
+        return res.json({error: 'Section not found'});
+      }
       var ios_channels = [];
       var android_channels = [];
 
@@ -148,72 +156,108 @@ module.exports = {
       });
 
 
-      Question.findOne({id:question_id}).exec(function (err, question) {
-        Answer.find({question: question.id}).exec(function(err, answers) {
+      Question.findOne({id:question_id}).populate('answers').exec(function (err, question) {
+        if(err) {
+          return res.json({error: res.negotiate(err)})
+        }
+        if(!question) {
+          return res.json({error: 'Question not found'});
+        }
 
-          var pushInfo = {
-            "audience": {},
-            "notification": {
-               "alert": "Question Available",
-               "android": {
-                 "extra": {
-                   "question": question.text,
-                   "quiz_id": question.quiz,
-                   "quest_id": question.id,
-                   "type": question.type,
-                   "answer0": answers[0].text,
-                   "answer1": answers[1].text,
-                   "time_limit": question.duration
-                 }
-               },
-               "ios": {
-                 "extra": {
-                   "question": question.text,
-                   "quiz_id": question.quiz,
-                   "quest_id": question.id,
-                   "type": question.type,
-                   "answer0": answers[0].text,
-                   "answer1": answers[1].text,
-                   "time_limit": question.duration
-                 }
+        var pushInfo = {
+          "audience": {},
+          "notification": {
+             "alert": "Question Available",
+             "android": {
+               "extra": {
+                 "question": question.text,
+                 "quiz_id": question.quiz,
+                 "quest_id": question.id,
+                 "type": question.type,
+                 "time_limit": question.duration,
+                 "num_answers": question.answers.length,
+                 "answers": []
                }
-            },
-            "device_types": "all"
-          };
+             },
+             "ios": {
+               "extra": {
+                 "question": question.text,
+                 "quiz_id": question.quiz,
+                 "quest_id": question.id,
+                 "type": question.type,
+                 "time_limit": question.duration,
+                 "num_answers": question.answers.length,
+                 "answers": []
+               }
+             }
+          },
+          "device_types": "all"
+        };
+        //Do this a better way. Try this:
+        /*
+        pushInfo.notifcation.android.extra.answers = [];
+        question.answers.forEach(function(answer) {
+          pushInfo.notification.android.extra.answers.push(answer);
+        });
+        //Debug with Android to see how it will receive an array of answers objects
+        */
+        if(question.answers.length != 0) {
+          pushInfo.notification.android.extra.answer0 = question.answers[0].text;
+          pushInfo.notification.android.extra.answer1 = question.answers[1].text;
+          pushInfo.notification.ios.extra.answer0 = question.answers[0].text;
+          pushInfo.notification.ios.extra.answer1 = question.answers[1].text;
 
-          if(ios_channels.length != 0) {
-            pushInfo.audience.ios_channel = ios_channels;
+          pushInfo.notification.android.extra.answerId0 = question.answers[0].id;
+          pushInfo.notification.android.extra.answerId1 = question.answers[1].id;
+          pushInfo.notification.ios.extra.answerId0 = question.answers[0].id;
+          pushInfo.notification.ios.extra.answerId1 = question.answers[1].id;
+          if(question.answers.length > 2) {
+            pushInfo.notification.android.extra.answer2 = question.answers[2].text;
+            pushInfo.notification.ios.extra.answer2 = question.answers[2].text;
+
+            pushInfo.notification.android.extra.answerId2 = question.answers[2].id;
+            pushInfo.notification.ios.extra.answerId02 = question.answers[2].id;
           }
+          if(question.answers.length > 3) {
+            pushInfo.notification.android.extra.answer3 = question.answers[3].text;
+            pushInfo.notification.ios.extra.answer3 = question.answers[3].text;
 
-          if(android_channels.length != 0) {
-            pushInfo.audience.android_channel = android_channels;
+            pushInfo.notification.android.extra.answerId3 = question.answers[3].id;
+            pushInfo.notification.ios.extra.answerId3 = question.answers[3].id;
           }
+          if(question.answers.length > 4) {
+            pushInfo.notification.android.extra.answer3 = question.answers[4].text;
+            pushInfo.notification.ios.extra.answer3 = question.answers[4].text;
 
-          if(answers.length > 2) {
-            pushInfo.notification.android.extra.answer2 = answers[2].text;
-            pushInfo.notification.ios.extra.answer2 = answers[2].text;
+            pushInfo.notification.android.extra.answerId4 = question.answers[4].id;
+            pushInfo.notification.ios.extra.answerId4 = question.answers[4].id;
           }
-          if(answers.length > 3) {
-            pushInfo.notification.android.extra.answer3 = answers[3].text;
-            pushInfo.notification.ios.extra.answer3 = answers[3].text;
-          }
+        }
 
-          urbanAirshipPush.push.send(pushInfo, function (err, data) {
-              if (err) {
-                  // Handle error
-                  console.log(err);
-                  return;
-              }
-              console.log(data);
-              return res.json({
-                success: "Push sent"
-              });
+        if(ios_channels.length == 0 && android_channels.length == 0) {
+          return res.json({error: "There are no devices for that section"});
+        }
 
-          });
+        if(ios_channels.length != 0) {
+          pushInfo.audience.ios_channel = ios_channels;
+        }
+
+        if(android_channels.length != 0) {
+          pushInfo.audience.android_channel = android_channels;
+        }
+
+        urbanAirshipPush.push.send(pushInfo, function (err, data) {
+            if (err) {
+                // Handle error
+                console.log(err);
+                return;
+            }
+            console.log(data);
+            return res.json({
+              success: "Push sent"
+            });
         });
       });
-
-
     });
   },
 
@@ -266,6 +310,17 @@ module.exports = {
           return res.json(studentAnswer);
         });
       });
+    });
+  },
+
+  answerFreeResponse: function(req, res) {
+    console.log("Question ID: " + req.param('quest_id'));
+    console.log("Quiz ID: " + req.param('quiz_id'));
+    console.log("User Email: " + req.param('user_email'));
+    console.log("Answer: " + req.param('answer'));
+
+    Student.findOne({email : req.param('user_email')}).exec(function(err, student) {
+
     });
   }
 };
